3ec938b098cf3fa6c8df80e1e29e110c3e0e3813
Jørgen S. Dokken <dokken@simula.no>
2023-04-21 16:56:46 +0200

    Add license and update all examples.

--- examples/voxelize-mesh.py
+++ examples/voxelize-mesh.py
@@ -1,42 +1,48 @@
 """Create a voxel mask from FEniCS mesh
 """
 import argparse
-import dolfin
+from dolfin import Mesh, HDF5File, FunctionSpace, Function
 import nibabel
+import pathlib
 from tracerdiffusion.utils import function_to_image
 
 if __name__ == "__main__":
-        
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mesh", required=True, type=str, help="FEniCS mesh file")
-    parser.add_argument("--image", required=True, type=str, help="MRI to get the vox2ras matrices from")
-    parser.add_argument("--output", required=True, type=str, help="Where to store the mask. Example: ./meshmask.mgz")
+    parser.add_argument("--mesh", required=True,
+                        type=str, help="FEniCS mesh file")
+    parser.add_argument("--image", required=True, type=str,
+                        help="MRI to get the vox2ras matrices from")
+    parser.add_argument("--output", required=True, type=str,
+                        help="Where to store the mask. Example: ./meshmask.mgz")
     parserargs = vars(parser.parse_args())
 
     # Load data
     nii_img = nibabel.load(parserargs["image"])
+    mesh_path = pathlib.Path(parserargs["mesh"])
+    if not mesh_path.is_file():
+        raise RuntimeError(f"Could not find mesh file at: {mesh_path}")
 
-    if parserargs["mesh"].endswith(".xml"):
-        brainmesh = dolfin.Mesh(parserargs["mesh"])
+    if mesh_path.suffix == ".xml":
+        brainmesh = Mesh(str(parserargs["mesh"]))
     else:
-        brainmesh= dolfin.Mesh()
-        hdf = dolfin.HDF5File(brainmesh.mpi_comm(), parserargs["mesh"], "r")
+        brainmesh = Mesh()
+        hdf = HDF5File(brainmesh.mpi_comm(), str(mesh_path), "r")
         hdf.read(brainmesh, "/mesh", False)
         hdf.close()
 
     # Setup function
-    V = dolfin.FunctionSpace(brainmesh, "DG", 0) 
-    f = dolfin.Function(V)
+    V = FunctionSpace(brainmesh, "DG", 0)
+    f = Function(V)
 
     f.vector()[:] = 1
 
-    output_volume, output_arry = function_to_image(function=f, template_image=nii_img, extrapolation_value=0)
-
+    output_volume, output_arry = function_to_image(
+        function=f, template_image=nii_img, extrapolation_value=0)
 
     nibabel.save(output_volume, parserargs["output"])
 
-
-    print("*"* 80)
+    print("*" * 80)
     print("Script done, to view the result in freeview run")
     print("freeview " + parserargs["output"])
-    print("*"* 80)
+    print("*" * 80)
