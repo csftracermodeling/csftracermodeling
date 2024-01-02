5231e15fa067930fa92bf8aca8aa8ab0e4bac3a8
bzapf <bwazapf@gmail.com>
2024-01-02 07:04:02 +0100

    add changes from revision

--- scripts/mesh-generation/make_mesh.py
+++ scripts/mesh-generation/make_mesh.py
@@ -4,18 +4,18 @@ import meshio
 import pathlib
 
 
-def create_gwv_mesh(pial_stl, ventricles_stl, resolution, 
+def create_gwv_mesh(pial_stl, ventricles_stl, resolution,
                     output, remove_ventricles=True):
 
     assert os.path.isfile(pial_stl)
     assert os.path.isfile(ventricles_stl)
 
     # Create SVMTk Surfaces from STL files
-    pial  = svmtk.Surface(pial_stl)
+    pial = svmtk.Surface(pial_stl)
     ventricles = svmtk.Surface(ventricles_stl)
     surfaces = [pial, ventricles]
 
-    # Define identifying tags for the different regions 
+    # Define identifying tags for the different regions
     tags = {"pial": 1, "ventricle": 2}
 
     # Define the corresponding subdomain map
@@ -27,13 +27,13 @@ def create_gwv_mesh(pial_stl, ventricles_stl, resolution,
     domain = svmtk.Domain(surfaces, smap)
 
     domain.create_mesh(resolution)
-    
+
     # Remove subdomain with right tag from the domain
     if remove_ventricles:
         domain.remove_subdomain(tags["ventricle"])
-        
-    # Save the mesh  
-    domain.save(output) 
+
+    # Save the mesh
+    domain.save(output)
 
 
 if __name__ == "__main__":
@@ -47,9 +47,10 @@ if __name__ == "__main__":
                     resolution=16,
                     output=outputfile, remove_ventricles=True)
 
-    os.system("meshio-convert " + outputfile + " " + outputfile.replace(".mesh", ".xml"))
-    os.system("meshio-convert " + outputfile + " " + outputfile.replace(".mesh", ".xdmf"))
-
+    os.system("meshio convert " + outputfile + " " +
+              outputfile.replace(".mesh", ".xml"))
+    os.system("meshio convert " + outputfile + " " +
+              outputfile.replace(".mesh", ".xdmf"))
 
     try:
         from fenics import *
@@ -61,21 +62,21 @@ if __name__ == "__main__":
 
         File(boundarymeshfile) << meshboundary
 
-        os.system("meshio-convert " + boundarymeshfile + " " + boundarymeshfile.replace(".xml", ".xdmf"))
-        os.system("meshio-convert " + boundarymeshfile + " " + boundarymeshfile.replace(".xml", ".stl"))
-
-
+        os.system("meshio convert " + boundarymeshfile + " " +
+                  boundarymeshfile.replace(".xml", ".xdmf"))
+        os.system("meshio convert " + boundarymeshfile + " " +
+                  boundarymeshfile.replace(".xml", ".stl"))
 
     except ModuleNotFoundError:
         pass
-
 
     print()
     print("*" * 80)
     print("Meshing done, to view the result in paraview run")
     print("paraview " + outputfile.replace(".mesh", ".xdmf"))
-    
+
     print()
     print("To view the mesh surface in freeview, run")
-    print("freeview ./data/freesurfer/mri/aseg.mgz -f " + boundarymeshfile.replace(".xml", ".stl"))
+    print("freeview ./data/freesurfer/mri/aseg.mgz -f " +
+          boundarymeshfile.replace(".xml", ".stl"))
     print("*" * 80)
