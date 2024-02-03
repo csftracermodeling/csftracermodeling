44528d822e5914d254b95bb12e4f64ba8ee674c2
bzapf <{69807673}+{bzapf}@users.noreply.github.com>
2024-02-03 08:12:16 +0100

    add env file and rename jax folder

---
+++ src/tracerdiffusion/jax_utils/slim_natgrad/derivatives.py
@@ -0,0 +1,21 @@
+import jax.numpy as jnp
+from jax import grad
+
+
+def del_i(g, argnum: int = 0):
+    """
+    Partial derivative for a function of signature (d,) ---> ().
+    Intended to use when defining PINN loss functions.
+
+    """
+
+    def g_splitvar(*args):
+        x_ = jnp.array(args)
+        return g(x_)
+
+    d_splitvar_di = grad(g_splitvar, argnum)
+
+    def dg_di(x):
+        return d_splitvar_di(*x)
+
+    return dg_di
